a5d83e6890ca0afa361b6faa1ac046b062fce27d
abouelabbas <36607503+abouelabbas@users.noreply.github.com>
2021-09-15 12:58:39 +0200

    Making modifications

--- resources/views/Admin/MyCourses/session-prog.blade.php
+++ resources/views/Admin/MyCourses/session-prog.blade.php
@@ -38,9 +38,7 @@
 
             <div class="ms-panel-body">
               <div class="d-flex justify-content-end">
-                <form action='{{url("/Session/$Session->SessionId/Progress/File")}}' method="get">
-                  <input type="button" value="Download all tasks" class="btn btn-success mb-2 mt-0">
-                </form>&nbsp;
+                <a href="{{url("/Session/$Session->SessionId/Progress/File")}}" class="btn btn-success mb-2 mt-0">Download all tasks</a>&nbsp;
                 <input type="submit" value="Save" id="savesessionprog" class="btn btn-success mb-2 mt-0">
                 
 
