0f3eaf8d8caadf407f98e63c685f52c4fced9e86
Abdallah Ahmed Ali <abdallahahmedali@outlook.com>
2023-09-11 21:41:14 +0300

    chgs 4

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -52,7 +52,7 @@ class GeneralController extends Controller
                 ['HasTask', '!=', 0],
                 ['IsDone', '=', 1],
                 ['IsCancelled', '=', null],
-                ['SessionTask', '!=', null]
+                // ['Student', '!=', null]
             ])->get();
 
         $ExamGrades = DB::table('examgrades')
